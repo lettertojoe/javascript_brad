c68a5d82e30a6c256238941e0469bd9d0bbd16b6
lettertojoe <lettertojoe@yahoo.com>
2019-02-23 23:09:12 +1100

    type conversion

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
-const person = {
-  name: 'Joseph',
-  age: 36
-}
+// let val;
 
-console.log(typeof(person.age));
+// val = 5;
+
+//console.log(val);
+// console.log(typeof(val));
+
+
+//To number
+let val;
+
+val = new Date();
+val  = Number('5');
+
+console.log(typeof(val));
+
+console.log(val);
