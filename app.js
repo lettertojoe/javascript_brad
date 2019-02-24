1d1a8a23303fb354188e0f07a0869a4dea610ce5
lettertojoe <lettertojoe@yahoo.com>
2019-02-24 19:02:26 +1100

    Date and Time objects

--- app.js
+++ app.js
@@ -15,3 +15,26 @@ val  = Number('5');
 console.log(typeof(val));
 
 console.log(val);
+
+const person = {
+  firstName: 'Steve',
+  lastName: 'Smith',
+  age: 30,
+  email: 'steve@aol.com',
+  hobbies: ['music', 'sports'],
+  address:{
+    city: 'Miami',
+    state: 'FL'
+  },
+  getBirthYears: function(){
+    return 1987;
+  }
+}
+
+let obj;
+
+obj = person;
+
+console.log(obj);
+
+//Date and Time
