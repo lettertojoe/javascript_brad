bc9c4c18ccb1b19a14433e900032a6698e39e4c9
lettertojoe <lettertojoe@yahoo.com>
2019-03-02 20:05:43 +1100

    for statement, scope declaration

--- app.js
+++ app.js
@@ -35,5 +35,46 @@
 
 // obj = person;
 
-// console.log(obj);
+// console.log(obj)
 
+//FOR LOOP
+// for (let i = 0; i < 5; i++){
+//   // if(i === 3){
+//   //   continue; // Go to the next iteration without running any other code.
+//   //   console.log(i);
+//   // }
+//   if (i === 3){
+//     break; // This breaks the loop completely; jumping out of the for loop
+//   }
+//   console.log(i);
+// }
+
+var a = 1;
+
+let b = 2;
+
+const c = 3;
+
+// function test(){
+//   var a = 4;
+
+//   let b = 5;
+
+//   const c = 6;
+
+//   console.log('Function scope' + ' ', a, b, c);
+// }
+
+// test();
+
+if (true){
+  var a = 4;
+
+  let b = 5;
+
+  const c = 6;
+
+  console.log('Function scope' + ' ', a, b, c);
+}
+
+console.log('Global scope' + ' ', a, b, c);
