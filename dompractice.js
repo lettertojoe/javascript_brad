7031e679c4f3bf62617bf0de5e02655eba31ab9c
lettertojoe <lettertojoe@yahoo.com>
2019-03-21 23:41:52 +1100

    Practising adding task

--- dompractice.js
+++ dompractice.js
@@ -59,3 +59,17 @@
 //   console.log(e);
 // }
 
+const form = document.querySelector('form');
+
+const taskInput =  document.getElementById('task');
+
+form.addEventListener('submit', runEvent);
+
+function runEvent (e){
+  e.preventDefault();
+  console.log(`EVENT TYPE: ${e.type}`);
+}
+
+localStorage.setItem('name','John');
+sessionStorage.setItem('name','John');
+
---
+++ domproject.js
@@ -0,0 +1,44 @@
+//Define UI vars
+const form = document.querySelector('#task-form');
+const taskList = document.querySelector('.collection');
+const clearBtn = document.querySelector('clear-tasks');
+const filter = document.querySelector('filter');
+const taskInput = document.querySelector('#task');
+
+//Load all event listeners
+loadEventListeners();
+
+//Load all event listeners function
+function loadEventListeners(){
+  //Add task event
+  form.addEventListener('submit', addTask);
+}
+
+//Add Task
+function addTask(){
+  if(taskInput.value === ''){
+    alert('Please add task');
+  } else{
+
+    //create li element
+    const li = document.createElement('li');
+    // Add class to li
+    li.className = 'collection-item';
+    //create a textnode
+    li.appendChild(document.createTextNode('taskInput.value'));
+    //Create a new link element
+    const link = document.createElement('a');
+    //add Class
+    link.className = 'delete-item secondary-content';
+    link.innerHTML = '<i class="fa fa-remove"></i>';
+    //Append the link to li
+    li.appendChild(link);
+    //Append li to UI
+    taskList.appendChild(li);
+    //clear input
+    taskInput = '';
+
+
+    e.preventDefault();
+  }
+}
